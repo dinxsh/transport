df3881f318c7dd5946d38bb2d1bb13ec34919ed3
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-08 20:25:57 +0530

    updated frontend dir and its html content pages

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,21 +1,15 @@
 const express = require("express")
 const router = express.Router()
 var bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get("/", (req,res)=>{
     res.render("../../frontend/order/index.html")
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://blaze:Omshanti@2005@cluster0.hwwyw.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://blaze:Omshanti%402005@cluster0.hwwyw.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("Connexted to db")
-  client.close();
-});
 
 router.post('/', urlencodedParser, (req, res) => {
     console.log('Got body:', req.body);
@@ -23,6 +17,7 @@ router.post('/', urlencodedParser, (req, res) => {
     console.log('Number:', req.body.number);
     console.log('Address:', req.body.address);
     res.send(" Name: " +  req.body.name +  " Number: " +  req.body.number + " Address: "  + req.body.address)    
+    InsertOrder(req.body.name, req.body.number, req.body.address)
 });
 
 module.exports = router
