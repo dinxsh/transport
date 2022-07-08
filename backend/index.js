b82c2cde1fb750335c6f5810b87ca557c5f928f4
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-08 16:31:19 +0530

    updated routes

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,6 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
-
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,4 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
+router.get("/", (req,res)=>{
+    res.render("../../frontend/order/index.html")
+})
+
 module.exports = router
